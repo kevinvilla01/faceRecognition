5079a8df6dda203e5f5e36d2d5c05017a3fae84b
kevinvilla01 <kevin01villa0101@gmail.com>
2024-10-27 17:41:38 -0600

    leds code arduino

--- conection.py
+++
@@ -1,8 +0,0 @@
-import serial
-import time
-
-ser = serial.Serial('COM3', 9600, timeout=1) #Puerto COM3 en 9600 baudios
-
-ser.write(b'p') #Enviar datos a arduino
-
-ser.close() #Cerrar la conexion 
